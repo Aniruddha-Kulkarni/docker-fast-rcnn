358e651024e1cd7b39e80abf6bb3752436acf26c
Duc Hoang Nguyen <lazycoder97@gmail.com>
2017-11-20 16:59:17 +0700

    add name and volume for container

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 xhost +local:
 
 docker run --rm -it \
+    --name fast-rcnn \
     -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0 \
+    -v $(pwd)/shared:/cv/shared \
     -p 8888:8888 \
     fast-rcnn \
     jupyter notebook --ip='0.0.0.0' --no-browser --allow-root
